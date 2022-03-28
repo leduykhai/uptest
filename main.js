1c6e337ec30936e2ba2fb9f5bc78d36fc95611ab
LESIDUYKHAI <leduykhai00@gmail.com>
2022-03-28 16:09:34 +0700

    Upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// var Name = 'Duy Khải';
+// alert(Name);
+var myInfo = {
+    name: 'Duy Khai',
+    age: 22,
+    addDress: 'Quang Tri'
+}
+console.log(myInfo)
+
+var inFo = {
+    name: 'khai',
+    age: 22
+}
+
+var result = inFo.age > 0 ? `${inFo.age} Tuổi` : '???'
+console.log(result)
